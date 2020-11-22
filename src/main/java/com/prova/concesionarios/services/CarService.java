906166cbc956639249280be3cf8832b9f07b00fc
rodergas14 <robertalmar@gmail.com>
2020-11-22 17:19:47 +0100

    Refactor car interface

--- src/main/java/com/prova/concesionarios/services/CarService.java
+++ src/main/java/com/prova/concesionarios/services/CarService.java
@@ -4,13 +4,44 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
 import com.prova.concesionarios.model.Car;
+import com.prova.concesionarios.repositories.CarRepository;
 
-public interface CarService {
-	public List<Car> findAll(Sort sort);
-	public Optional<Car> findById(Long id);
-	public Optional<Car> updateCar(Car car, Long id);
-	public Optional<Car> deleteCar(Long id);
-	public Car insertCar(Car car);
+@Service
+public class CarService {
+
+	private final CarRepository carRepostiory;
+
+	public CarService(CarRepository carJPARepository) {
+		this.carRepostiory = carJPARepository;
+	}
+
+	public Car insertCar(Car car) {
+		return carRepostiory.save(car);
+	}
+
+	public List<Car> findAll(Sort sort) {
+		return carRepostiory.findAll(sort);
+	}
+
+	public Optional<Car> findById(Long id) {
+		return carRepostiory.findById(id);
+	}
+
+	public Optional<Car> updateCar(Car c, Long id) {
+		return carRepostiory.findById(id).map(car -> {
+			return carRepostiory.save(car);
+		});
+
+	}
+
+	public Optional<Car> deleteCar(Long id) {
+		return carRepostiory.findById(id).map(car -> {
+			carRepostiory.deleteById(id);
+			return car;
+		});
+	}
+
 }
